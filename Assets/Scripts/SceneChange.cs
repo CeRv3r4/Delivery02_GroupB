088a013087a437d8e5e07790c223b3a08b1d78df
FurryGirl777 <amasipm@edu.tecnocampus.cat>
2025-02-12 21:15:43 +0100

    Wincondition

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Ending : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
